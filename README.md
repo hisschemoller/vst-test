3c43e90668d8213a29a8cbf2149f875cd19a5d3b
Wouter Hisschemöller <wouter@electricbrains.nl>
2023-02-25 18:13:40 +0100

    Https-localhost to serve over https.

--- README.md
+++ README.md
@@ -4,10 +4,27 @@ Testing the Elementary Audio plugin renderer.
 
 ## Setup
 
-```
+```bash
+# install dependencies
 npm init
-npm install --save @elemaudio/plugin-renderer
+
+# copy dev kit vst plugin, when asked where: /Library/Audio/Plug-Ins
 npx elem-copy-binaries
-# Where: /Library/Audio/Plug-Ins
-npm run serve
+
+# nss/certutils are needed for Firefox and Chrome.
+brew install nss
+
+# https-localhost to serve over https
+npm i -g --only=prod https-localhost
+
+# start server
+PORT=3000 HOST=127.0.0.1 serve src
 ```
+
+Then view in browser at https://127.0.0.1:3000/<br>
+Or open in VST host with the ElementaryDevKit VST plugin.
+
+## Documentation and tutorials
+
+* Build a native audio plugin with Elementary
+  * https://blog.logrocket.com/build-native-audio-plugin-elementary/
--- server.mjs
+++ server.mjs
@@ -1,16 +1,35 @@
 import express from 'express';
 import { resolve } from 'path';
+import https from 'https';
+import fs from 'fs';
 
 const app = express();
 const port = process.env.PORT || 3000;
 const rootDir = process.argv[2] || 'src';
+// const privateKey  = fs.readFileSync('key.pem', 'utf8');
+// const certificate = fs.readFileSync('cert.pem', 'utf8');
+// const credentials = { key: privateKey, cert: certificate };
+const options = {
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.crt'),
+  requestCert: false,
+  rejectUnauthorized: false,
+};
+
+https
+  .createServer(options, app)
+  .listen(port, () => {
+    console.log('\x1b[42m%s\x1b[0m', `                                      `);
+    console.log('\x1b[42m%s\x1b[0m', `     VST on https://127.0.0.1:${port}    `);
+    console.log('\x1b[42m%s\x1b[0m', `                                      `);
+  });
 
 // Set public folder as root
 app.use(`/`, express.static(resolve(rootDir)));
 
-// Listen for HTTP requests on port 3008
-app.listen(port, () => {
-  console.log('\x1b[42m%s\x1b[0m', `                                      `);
-  console.log('\x1b[42m%s\x1b[0m', `     VST on http://127.0.0.1:${port}     `);
-  console.log('\x1b[42m%s\x1b[0m', `                                      `);
-});
+// // Listen for HTTP requests on port 3008
+// app.listen(port, () => {
+//   console.log('\x1b[42m%s\x1b[0m', `                                      `);
+//   console.log('\x1b[42m%s\x1b[0m', `     VST on http://127.0.0.1:${port}     `);
+//   console.log('\x1b[42m%s\x1b[0m', `                                      `);
+// });
